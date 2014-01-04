f927ba7b0c12f0f340eff16c59f886c7294a2326
Andrews Medina <andrewsmedina@gmail.com>
2014-01-04 01:43:47 -0200

    added a new object to manager redis.

--- redisapi.py
+++ redisapi.py
@@ -25,44 +25,70 @@ def coalesce(default, *args):
     return default
 
 
+class RedisManager(object):
+    def add_instance(self):
+        pass
+
+    def bind(self):
+        host = coalesce(server, "REDIS_PUBLIC_HOST")
+        port = coalesce("6379", "REDIS_SERVER_PORT")
+        result = {
+            "REDIS_HOST": host,
+            "REDIS_PORT": port,
+        }
+        pswd = os.environ.get("REDIS_SERVER_PASSWORD")
+        if pswd:
+            result["REDIS_PASSWORD"] = pswd
+        return result
+
+    def unbind(self):
+        pass
+
+    def remove_instance(self):
+        pass
+
+    def status(self):
+        passwd = os.environ.get("REDIS_SERVER_PASSWORD")
+        kw = {"host": server}
+        if passwd:
+            kw["password"] = passwd
+        try:
+            conn = redis.Connection(**kw)
+            conn.connect()
+        except Exception as e:
+            return str(e), 500
+        return "", 204
+
+
 @app.route("/resources/<name>", methods=["POST"])
 def bind(name):
-    host = coalesce(server, "REDIS_PUBLIC_HOST")
-    port = coalesce("6379", "REDIS_SERVER_PORT")
-    result = {
-        "REDIS_HOST": host,
-        "REDIS_PORT": port,
-    }
-    pswd = os.environ.get("REDIS_SERVER_PASSWORD")
-    if pswd:
-        result["REDIS_PASSWORD"] = pswd
+    manager = RedisManager()
+    result = manager.bind()
     return json.dumps(result), 201
 
 
 @app.route("/resources/<name>/hostname/<host>", methods=["DELETE"])
 def unbind(name, host):
+    manager = RedisManager()
+    manager.unbind()
     return "", 200
 
 
 @app.route("/resources", methods=["POST"])
 def add_instance():
+    manager = RedisManager()
+    manager.add_instance()
     return "", 201
 
 
 @app.route("/resources/<name>", methods=["DELETE"])
 def remove_instance(name):
+    manager = RedisManager()
+    manager.remove_instance()
     return "", 200
 
 
 @app.route("/resources/<name>/status", methods=["GET"])
 def status(name):
-    passwd = os.environ.get("REDIS_SERVER_PASSWORD")
-    kw = {"host": server}
-    if passwd:
-        kw["password"] = passwd
-    try:
-        conn = redis.Connection(**kw)
-        conn.connect()
-    except Exception as e:
-        return str(e), 500
-    return "", 204
+    manager = RedisManager()
+    return manager.status()
