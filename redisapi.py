d4e95c996dd90f66dc081072b63fe1e3278583c6
Andrews Medina <andrewsmedina@gmail.com>
2014-01-07 16:57:41 -0200

    implemented manager

--- redisapi.py
+++ redisapi.py
@@ -11,38 +11,37 @@ from managers import RedisManager
 app = flask.Flask(__name__)
 
 
+def manager():
+    return RedisManager()
+
+
 @app.route("/resources/<name>", methods=["POST"])
 def bind(name):
-    manager = RedisManager()
-    result = manager.bind()
+    result = manager().bind()
     return json.dumps(result), 201
 
 
 @app.route("/resources/<name>/hostname/<host>", methods=["DELETE"])
 def unbind(name, host):
-    manager = RedisManager()
-    manager.unbind()
+    manager().unbind()
     return "", 200
 
 
 @app.route("/resources", methods=["POST"])
 def add_instance():
-    manager = RedisManager()
-    manager.add_instance()
+    manager().add_instance()
     return "", 201
 
 
 @app.route("/resources/<name>", methods=["DELETE"])
 def remove_instance(name):
-    manager = RedisManager()
-    manager.remove_instance()
+    manager().remove_instance()
     return "", 200
 
 
 @app.route("/resources/<name>/status", methods=["GET"])
 def status(name):
-    manager = RedisManager()
-    ok, msg = manager.is_ok()
+    ok, msg = manager().is_ok()
     if ok:
         return msg, 204
     return msg, 500
--- tests/test_redisapi.py
+++ tests/test_redisapi.py
@@ -4,29 +4,41 @@
 
 import json
 import unittest
+import os
 
 
 class RedisAPITestCase(unittest.TestCase):
 
-    def test_add_instance(self):
+    def remove_env(self, env):
+        if env in os.environ:
+            del os.environ[env]
+
+    def setUp(self):
+        os.environ["REDIS_SERVER_HOST"] = "localhost"
+        self.addCleanup(self.remove_env, "REDIS_SERVER_HOST")
         import redisapi
-        app = redisapi.app.test_client()
-        response = app.post("/resources")
+        self.app = redisapi.app.test_client()
+
+    def test_manager(self):
+        from managers import RedisManager
+        from redisapi import manager
+        self.assertIsInstance(manager(), RedisManager)
+
+    def test_add_instance(self):
+        response = self.app.post("/resources")
         self.assertEqual(201, response.status_code)
         self.assertEqual("", response.data)
 
     def test_remove_instance(self):
-        import redisapi
-        app = redisapi.app.test_client()
-        response = app.delete("/resources/myinstance")
+        response = self.app.delete("/resources/myinstance")
         self.assertEqual(200, response.status_code)
         self.assertEqual("", response.data)
 
     def test_bind(self):
-        import redisapi
-        app = redisapi.app.test_client()
-        response = app.post("/resources/myinstance",
-                            data={"hostname": "something.tsuru.io"})
+        response = self.app.post(
+            "/resources/myinstance",
+            data={"hostname": "something.tsuru.io"}
+        )
         self.assertEqual(201, response.status_code)
         j = json.loads(response.data)
         self.assertEqual({"REDIS_HOST": "localhost", "REDIS_PORT": "6379"}, j)
